a74a0af84e5205d809373979d0b30389bb212439
hunglp9397 <lephihung0997@gmail.com>
2023-12-10 01:54:51 +0700

    order-service done

---
+++ order-service/src/main/java/com/hunglp/orderservice/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.hunglp.orderservice.controller;
+
+
+import com.hunglp.orderservice.dto.OrderRequest;
+import com.hunglp.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-service/src/main/java/com/hunglp/orderservice/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.hunglp.orderservice.service;
+
+
+import com.hunglp.orderservice.dto.OrderLineItemsDto;
+import com.hunglp.orderservice.dto.OrderRequest;
+import com.hunglp.orderservice.model.Order;
+import com.hunglp.orderservice.model.OrderLineItems;
+import com.hunglp.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
