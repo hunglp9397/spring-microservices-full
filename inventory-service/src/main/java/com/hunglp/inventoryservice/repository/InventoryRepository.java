1f162e0a09eb66770a388d2c02038ab8cc856be0
hunglp9397 <lephihung0997@gmail.com>
2023-12-10 11:41:07 +0700

    inventory service done

---
+++ inventory-service/src/main/java/com/hunglp/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.hunglp.inventoryservice.repository;
+
+
+import com.hunglp.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
